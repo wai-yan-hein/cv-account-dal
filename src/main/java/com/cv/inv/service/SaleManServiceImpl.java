6ae34b06e0d2775acfcbb41fcf9ba7df220c53ce
devcorevalue2 <devcorevalue2@gmail.com>
2021-01-07 17:29:04 +0630

    a

--- src/main/java/com/cv/inv/service/SaleManServiceImpl.java
+++ src/main/java/com/cv/inv/service/SaleManServiceImpl.java
@@ -5,6 +5,7 @@
  */
 package com.cv.inv.service;
 
+import com.cv.accountswing.service.SeqTableService;
 import com.cv.inv.dao.SaleManDao;
 import com.cv.inv.entity.SaleMan;
 import java.util.List;
@@ -22,11 +23,18 @@ public class SaleManServiceImpl implements SaleManService {
 
     @Autowired
     private SaleManDao dao;
+    @Autowired
+    private SeqTableService seqService;
 
     @Override
-    public SaleMan save(SaleMan saleMan) {
-        dao.save(saleMan);
-        return saleMan;
+    public SaleMan save(SaleMan sm) {
+         if (sm.getSaleManCode()== null || sm.getSaleManCode().isEmpty()) {
+            Integer macId = sm.getMacId();
+            String compCode = sm.getCompCode();
+            String getCode=getSaleManCode(macId, "Category", "-", compCode);
+            sm.setSaleManCode(getCode);
+        }
+        return dao.save(sm);
     }
 
     @Override
@@ -44,4 +52,11 @@ public class SaleManServiceImpl implements SaleManService {
         return dao.findById(id);
     }
 
+     private String getSaleManCode(Integer macId, String option, String period, String compCode) {
+
+        int seqNo = seqService.getSequence(macId, option, period, compCode);
+
+        String tmpCatCode = String.format("%0" + 2 + "d", macId) + "-" + String.format("%0" + 3 + "d", seqNo);
+        return tmpCatCode;
+    }
 }
